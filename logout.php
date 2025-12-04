ac275edc45b64a4125ff8491f53b66776e4a008a
Jul150704 <julienmanique6@gmail.com>
2025-12-04 21:42:18 +0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,30 @@
 <?php
+// logout.php
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
+// Unset all session variables
 $_SESSION = [];
 
+// If using cookies for the session ID, delete the session cookie
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
+
+// Destroy the session
 session_destroy();
 
-header("Location: Login.php?message=You have been logged out successfully");
-exit();
-?>
+// Redirect to login page
+header("Location: login.php?message=You have been logged out successfully");
+exit;
+
 
 
